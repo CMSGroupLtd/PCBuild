d709ee5175ea63146c2b64f27f009ff9f98b57ae
CMS Group Ltd <69903530+CMSGroupLtd@users.noreply.github.com>
2020-08-19 14:11:20 +0100

    Update PCSetup.ps1

--- PCSetup.ps1
+++ PCSetup.ps1
@@ -607,11 +607,11 @@ function LayoutDesign {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
         Exit
     }
-    Import-StartLayout -LayoutPath "c:\build\pcbuild-2\LayoutModification.xml" -MountPath $env:SystemDrive\
+    Import-StartLayout -LayoutPath "c:\build\pcbuild-3\LayoutModification.xml" -MountPath $env:SystemDrive\
     }
     
 function ApplyDefaultApps {
-    dism /online /Import-DefaultAppAssociations:c:\build\pcbuild-2\AppAssociations.xml
+    dism /online /Import-DefaultAppAssociations:c:\build\pcbuild-3\AppAssociations.xml
 }
 
 # Custom power profile used for our customers. Ensures systems do not go to sleep.
