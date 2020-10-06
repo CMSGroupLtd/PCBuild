a06e2b1df2ffd51d3471f4014ca316c04d93220c
CMS Group Ltd <69903530+CMSGroupLtd@users.noreply.github.com>
2020-10-06 15:40:29 +0100

    Update RunMe.bat

--- RunMe.bat
+++ RunMe.bat
@@ -7,4 +7,4 @@ powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powe
 REM The download.ps1 auto extracts the contents to c:\build. Wait 10 seconds for this to complete before executing the PCSetup.ps1 script
 timeout 10
 REM Execute the final build script
-powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PCBuild-7\PCSetup.ps1\"' -verb RunAs}"
+powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PCBuild-8\PCSetup.ps1\"' -verb RunAs}"
