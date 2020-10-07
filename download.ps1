53e8769637bfe9c874708166f022b18beb8b1c41
CMS Group Ltd <69903530+CMSGroupLtd@users.noreply.github.com>
2020-10-07 13:06:10 +0100

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy remotesigned
 function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/cmsgroupltd/pcbuild/archive/9.zip" -OutFile "C:\build\PCBuild-9.zip"
-Expand-Archive C:\build\PCBuild-9.zip -DestinationPath C:\build\
+Invoke-WebRequest -Uri "https://github.com/cmsgroupltd/pcbuild/archive/10.zip" -OutFile "C:\build\PCBuild-10.zip"
+Expand-Archive C:\build\PCBuild-10.zip -DestinationPath C:\build\
 }
 downloadbuilder
